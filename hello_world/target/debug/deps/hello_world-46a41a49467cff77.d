d6e89867e92e61ec92e33e2486511e1a826542c8
Qluqlu3 <plksmcgof@gmail.com>
2023-12-10 16:30:56 +0900

    -

---
+++ hello_world/target/debug/deps/hello_world-46a41a49467cff77.d
@@ -0,0 +1,5 @@
+/Users/kay/program/ts/algo/rust/hello_world/target/debug/deps/hello_world-46a41a49467cff77: src/main.rs
+
+/Users/kay/program/ts/algo/rust/hello_world/target/debug/deps/hello_world-46a41a49467cff77.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/kay/program/ts/algo/rust/hello_world/target/debug/hello_world: /Users/kay/program/ts/algo/rust/hello_world/src/main.rs
